014ef41a2f3f24f19ce4f6a6f213a8ee309814fd
Steven Morrison <stevenmorrimorrison@hotmail.co.uk>
2017-01-09 09:37:47 +0000

    SM: data structure & dummy data setup.

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        categories: [
+            {
+                name: "Cat1",
+                tasks: [
+                    "Task 1",
+                    "Task 2",
+                    "Task 3"
+                ]
+            },
+            {
+                name: "Cat2",
+                tasks: [
+                    "Task 1",
+                    "Task 2",
+                    "Task 3",
+                    "Task 4"
+                ]
+            }
+        ]
+    }
+});
