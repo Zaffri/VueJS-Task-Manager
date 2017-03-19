f5027408b572162f53a545f20171fa0cdb19be71
Steven Morrison <stevenmorrimorrison@hotmail.co.uk>
2017-03-19 18:30:41 +0000

    SM: Upgraded src files added.

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 /* --------------------------------------------------------
 	VueJS - Modal Component
-	Version: 	0.0.1
+	Version: 	0.0.3
 	Author: 	Steven Morrison
 	Website:	www.zaffri.com
 	GitHub:		github.com/Zaffri
@@ -10,8 +10,16 @@ var ZaffriModal = Vue.component('zaffri-modal', {
     template: "#zaffri-modal-template",
     props: ['data'],
     methods: {
-        hideModal: function(action = null) {
-            this.$emit('hide_modal_emit', action);
+        closeModal: function(action = null) {
+            var callbackData = {};
+
+            if(this.data.callbackData != undefined) callbackData = this.data.callbackData;
+            if(this.data.confirmCallback != undefined) this.confirmCallback(action, callbackData);
+
+            this.data.visible = false;
+        },
+        confirmCallback: function(action, callbackData) {
+            this.data.confirmCallback(action, callbackData);
         }
     }
 });
